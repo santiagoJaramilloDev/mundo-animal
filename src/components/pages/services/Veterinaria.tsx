c1004a3dd5128d32b19b951a3df43744ff78ab03
santiagoJaramilloDev <76961534+santiagoJaramilloDev@users.noreply.github.com>
2025-02-07 17:22:46 -0500

    update veterinaria.tsx

--- src/components/pages/services/Veterinaria.tsx
+++ src/components/pages/services/Veterinaria.tsx
@@ -4,11 +4,6 @@ export const Veterinaria = () => {
       <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
         <div className="px-6 pt-16 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pb-56 xl:col-span-6">
           <div className="mx-auto max-w-2xl lg:mx-0">
-            <img
-              className="h-11"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
             <h1 className="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">
               Agenda ya tu cita veterinaria
             </h1>
